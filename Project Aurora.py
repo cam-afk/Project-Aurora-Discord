b77f91f5dd3dd1296dfd7655e1cb6a4e96c1dfd1
Cameron Kihnemann <campk@Camerons-MacBook-Air.local>
2025-06-03 23:23:22 -0500

    initital commit

---
+++ Project Aurora.py
@@ -0,0 +1,56 @@
+# Project Aurora - A Discord Bot for the Aurora Community
+
+import discord  
+from discord.ext import commands
+
+import pytz
+from datetime import datetime
+# Define the timezone for the bot
+
+import asyncio
+
+# Define the bot's command prefix and intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent
+
+bot = commands.Bot(command_prefix='!', intents=intents) 
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+          
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@bot.command()
+async def timedylon(ctx):
+    
+    loading_msg = await ctx.send("Calculating Dylon's time...")
+
+    await asyncio.sleep(2)
+
+    await ctx.message.delete()  # Delete the command message
+    await loading_msg.delete()
+
+    dylon = pytz.timezone("Europe/Paris")  # Set the timezone to Aviano - For Dylon
+    now = datetime.now(dylon)
+    formatted_time = now.strftime("%H:%M:%S")
+
+    await ctx.send(f"{ctx.author.mention}, The current time for Dylon is: {formatted_time}")
+
+@bot.command()
+async def timedallas(ctx):
+
+    await ctx.message.delete()
+
+    dallas = pytx.timezone("Central/America") # set timezone for central america
+    now= datetime.now(dallas)
+    formatted_time = now.strftime("%H:%M:%S")
+
+    await ctx.send(f"{ctx.author.mention}, The current time in CST is: {formatted_time}")
+
+
+
+bot.run('MTI3MjYzODUzMDY2NDkyNzI0Mg.G9_yPW.t8CoLkHXQ2DY0V3g0GpnPMU2fzA_BUhyk3kktk')
+
