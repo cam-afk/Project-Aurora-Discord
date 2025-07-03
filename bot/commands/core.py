cfe1176fbb6219f82e02fa3925bbfeeaf00dd0ca
Cam <cameronkihnemann@gmail.com>
2025-07-03 10:51:23 -0500

    added more commmands and cleaned up code

--- bot/commands/core.py
+++ bot/commands/core.py
@@ -2,13 +2,19 @@ import pytz
 import asyncio
 import random
 import discord
+import os
 from datetime import datetime
 from discord.ext import commands
 from discord import app_commands
+from dotenv import load_dotenv
 from . import agents  # from the same folder import agents
-from . import nealinsults
+from . import nealinsults # same as above
 
-GUILD_ID = discord.Object(id=1279154864697511956)
+load_dotenv()
+USER = os.getenv("USER")
+SERVER = os.getenv("ID")
+
+GUILD_ID = discord.Object(id=SERVER)
 
 @commands.command()
 async def ping(ctx):
@@ -41,14 +47,23 @@ async def timedallas(ctx):
 
     await ctx.send(f"{ctx.author.mention}, The current time in CST is: {formatted_time}")
 
+@commands.command()
+async def timezulu(ctx):
+        await ctx.message.delete()
+
+        zulu = pytz.timezone("Canada/Atlantic") #timezone for nathan "canadianzulu "
+        now = datetime.now(zulu)
+        formatted_time = now.strftime("%H:%M")
+
+        await ctx.send(f"{ctx.author.mention}, The current time for Zulu is: {formatted_time}")
 
 ### literally to make sure im running from terminal and not docker
 
-#@commands.command()
-#async def trun(ctx):
-    #await ctx.message.delete()
+@commands.command()
+async def trun(ctx):
+    await ctx.message.delete()
 
-    #await ctx.send(f'Hey {ctx.author.mention}, were now running from the terminal!!')
+    await ctx.send(f'Hey {ctx.author.mention}, were now running from the terminal!!')
 
 @commands.command()
 async def valrandom(ctx):
@@ -61,7 +76,7 @@ async def valrandom(ctx):
 
 
 class ValButton(discord.ui.View): #creates a class called ValButton that subclasses discord.ui.View
-    @discord.ui.button(label="Choose an Agent!", style=discord.ButtonStyle.primary) # create a  button with a emoji label that says "🧺 Choose an Agent!" with color burple 
+    @discord.ui.button(label="Choose an Agent!", style=discord.ButtonStyle.primary) # create a  button with a emoji label that says "Choose an Agent!" with color burple 
     async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
         random_agent = random.choice(agents.agent_list)
         await interaction.response.send_message(f"{interaction.user.mention}, Looks like you have to play {random_agent}!")
@@ -73,7 +88,7 @@ class Core(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
 
-    @app_commands.command(name="valrandom", description = "Press the button to get a random agent!!")
+    @app_commands.command(name="valrandom", description = "Get a random agent for your next game!!")
     @app_commands.guilds(GUILD_ID)
     async def RandomAgent(self, interaction: discord.Interaction):
         print("Slash command triggered")
@@ -83,8 +98,21 @@ async def setup(bot):
     await bot.add_cog(Core(bot))
 
 @commands.command()
-async def insultneal(ctx):
+async def insult(ctx, member: discord.Member = None ):
     await ctx.message.delete()
-    neal = await ctx.bot.fetch_user(493662983147356160)
-    nealinsult = random.choice(nealinsults.neal_insults)
-    await ctx.send(f"{neal.mention}, {nealinsult}")
+
+    if member is None:
+        member = await ctx.bot.fetch_user(USER)
+    
+    insult_line = random.choice(nealinsults.neal_insults)
+    await ctx.send(f"{member.mention}, {insult_line}")
+
+
+
+
+    #### @commands.command()
+#async def insultneal(ctx):
+    #await ctx.message.delete()
+    #neal = await ctx.bot.fetch_user(493662983147356160)
+    #nealinsult = random.choice(nealinsults.neal_insults)
+    #await ctx.send(f"{neal.mention}, {nealinsult}")
--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 # Project Aurora - A Discord Bot for the Aurora Community
+# Bot made by camps
 
 import discord
 from discord.ext import commands
@@ -16,6 +17,7 @@ import os
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN") # defines token
+SERVER = os.getenv("ID")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,16 +28,19 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 bot.add_command(core.ping)
 bot.add_command(core.timedylon)
 bot.add_command(core.timedallas)
-#bot.add_command(core.trun)
+bot.add_command(core.trun)
 bot.add_command(core.valrandom)
-bot.add_command(core.insultneal)
+bot.add_command(core.insult)
+bot.add_command(core.timezulu)
 
-GUILD_ID = discord.Object(id=1279154864697511956)
+GUILD_ID = discord.Object(id=SERVER)
 
 @bot.event
 async def on_ready():
     await bot.load_extension("commands.core")  # First load the cog
     await bot.tree.sync(guild=GUILD_ID)        # Then sync the slash commands
     print(f'Logged in as {bot.user.name}')
+    print('we online mf!')
+
 
 bot.run(TOKEN)
